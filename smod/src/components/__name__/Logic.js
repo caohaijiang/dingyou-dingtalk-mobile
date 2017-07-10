28eb6625ae2015ce9ee6b845ffa756278cd321a6
link <916969890@qq.com>
2017-07-10 16:48:01 +0800

    增加有状态组件

---
+++ smod/src/components/__name__/Logic.js
@@ -0,0 +1,11 @@
+import PageConst from './Const';
+
+export default {
+    //默认状态
+    defaults(props) {
+        return { 
+
+        }
+    },
+
+};
---
+++ smod/src/components/__name__/__Name__Component.jsx
@@ -0,0 +1,22 @@
+require('./<%=Name%>.less');
+import { Component } from 'refast';
+import logic from './Logic';
+
+
+
+class CardTabbarSelf extends Component {
+    constructor(props) {
+        super(props, logic);
+       
+    }
+
+ 
+    render() {
+
+        return (<div>
+            
+        </div>)
+    }
+}
+
+export default CardTabbarSelf ;
